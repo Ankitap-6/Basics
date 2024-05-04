e1ff9051faee51031fb2898898e479d01ef6f73f
Ankita Patra <ankita.patra@videojet.com>
2024-05-05 00:24:00 +0530

    Initial commit

---
+++ src/DataType.java
@@ -0,0 +1,16 @@
+public class DataType {
+    public static void main(String[] args){
+        int num= 2;
+        float dec_val= 5.9f;
+        double d1= 123.4;
+        String text = "Hello world";
+        char c= 'A';
+
+        System.out.println("Integer:" + num);
+        System.out.println("float:" + dec_val);
+        System.out.println("double:" + d1);
+        System.out.println("String:" + text);
+        System.out.println("char:" + c);
+
+    }
+}
